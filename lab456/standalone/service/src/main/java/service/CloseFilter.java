4860a3904b4392e7c58febc6a34e706d5aafefa4
Dan Bogomolov <Bogomolov.Daniil1997@yandex.ru>
2019-04-28 21:21:59 +0300

    add throttling logic

--- lab456/standalone/service/src/main/java/service/CloseFilter.java
+++ lab456/standalone/service/src/main/java/service/CloseFilter.java
@@ -3,13 +3,11 @@ package service;
 import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerResponse;
 import com.sun.jersey.spi.container.ContainerResponseFilter;
-import service.exceptions.ThrottlingException;
 
 public class CloseFilter implements ContainerResponseFilter {
 
     @Override
-    public ContainerResponse filter(ContainerRequest request, ContainerResponse response)
-            throws ThrottlingException {
+    public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
         ThrottlingLogic.removeConnection();
         return response;
     }
--- lab456/standalone/service/src/main/java/service/ThrottlingLogic.java
+++ lab456/standalone/service/src/main/java/service/ThrottlingLogic.java
@@ -11,16 +11,17 @@ public class ThrottlingLogic {
     private static final Integer MAX_CONNECTIONS_COUNT = 3;
     private static final Logger LOGGER = Logger.getLogger(ThrottlingLogic.class.getName());
 
-    public static void addConnection() throws ThrottlingException{
-
-        if(connectionCount.incrementAndGet() > MAX_CONNECTIONS_COUNT) {
-            LOGGER.log(Level.INFO, "connection pull is full, count of connections:  " + connectionCount.get());
+    public static void addConnection() throws ThrottlingException {
+        int count = connectionCount.incrementAndGet();
+        if (count > MAX_CONNECTIONS_COUNT) {
+            LOGGER.log(Level.INFO, "connection pull is full, count of connections:  " + count);
             throw ThrottlingException.DEFAULT_INSTANCE;
         }
-        LOGGER.log(Level.INFO, "connection is added, count of connections: " + connectionCount.get());
+        LOGGER.log(Level.INFO, "connection is added, count of connections: " + count);
     }
-    public static void removeConnection() throws ThrottlingException{
-        connectionCount.decrementAndGet();
-        LOGGER.log(Level.INFO, "connection is realised, count of connections: " + connectionCount.get());
+
+    public static void removeConnection() {
+        int count = connectionCount.decrementAndGet();
+        LOGGER.log(Level.INFO, "connection is realised, count of connections: " + count);
     }
 }
